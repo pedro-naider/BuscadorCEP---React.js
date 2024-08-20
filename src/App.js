8731bcaa1408c432d9e6c5c4a021169498b720a4
Pedro Henrique de Oliveira Naider <pedronaider@gmail.com>
2024-08-20 14:36:06 -0300

    Buscador de CEP React

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { HiSearch } from "react-icons/hi";
+import "./style.css";
+import api from "./services/api";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    // 01310930/json/
+
+    if (input === "") {
+      alert("Preencha algum CEP!!");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Erro ao buscar este CEP");
+      setInput("");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de CPF</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <HiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default App;
